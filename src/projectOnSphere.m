ced9199594525b66eafab349f3803bf9655397ed
khushhallchandra <kcm00000@gmail.com>
2016-04-24 23:44:08 +0530

    Added data

---
+++ src/projectOnSphere.m
@@ -0,0 +1,15 @@
+% This function will take an image 
+% And the registration parameters
+% Convert it to spherical system
+%function [ out ] = projectOnSphere(img,R,f)
+%out  = img
+
+img = imread('../data/building1.JPG');
+
+[x,y] = size(img);
+
+for i = 1:x
+	for j = 1:y
+         
+	end
+end
