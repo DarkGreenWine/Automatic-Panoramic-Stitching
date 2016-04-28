4109a5dc3fea2fb151af5be910485a90f29b17ff
khushhallchandra <kcm00000@gmail.com>
2016-04-28 23:48:32 +0530

    Added

--- src/projectOnSphere.m
+++ src/projectOnSphere.m
@@ -1,16 +1,46 @@
 % This function will take an image 
 % And the registration parameters
 % Convert it to spherical system
-%function [ out ] = projectOnSphere(img,R,f)
-%out  = img
+function [ out ] = projectOnSphere(img,f)
+%I = imread('building1.JPG');
+%f = 400;
+width = size(I,2);
+height = size(I,1);
 
-img = imread('../data/building1.JPG');
+% Calculate image center. Middle pixel.
+x_center = floor(width/2);
+y_center = floor(height/2);
 
-[x,y] = size(img);
-theta_phi = zeros(2*x,2*y);
+unwrapped = zeros(floor(f), floor(f), 3);
 
-for i = 1:x
-	for j = 1:y
-         
-	end
+for i = 1:width
+    for j = 1:height
+        curr_x = i-x_center; 
+        curr_y = j-y_center; 
+
+	    mag_XYZ = sqrt(curr_x^2 + curr_y^2 + F^2);
+        phi = asin(curr_y/mag_XYZ);
+        theta = asin(curr_x/(mag_XYZ*cos(phi)));
+
+        x_sph = round(f*theta + x_center);
+        y_sph = round(f*phi + y_center);
+        unwrapped(y_sph,x_sph,:) = I(j,i,:);
+    end
 end
+out = uint8(unwrapped);
+imshow(out);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
